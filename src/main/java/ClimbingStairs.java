9ae992553dca584b465a7fe7d61a5aa3394980c1
vvaidya1 <vaibhav.vaidya11@gmail.com>
2022-06-12 20:46:56 +0100

    InterviewBit Java Solutions

---
+++ src/main/java/ClimbingStairs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/*
+ * https://www.interviewbit.com/problems/climbing-stairs/
+ * O(n) time | O(n) space - where n is the length of input array
+ */
+public class ClimbingStairs {
+    public int solve(ArrayList<Integer> A) {
+        if (A == null || A.size() == 0)
+            return 0;
+
+        int[] cost = new int[A.size()];
+        cost[0] = A.get(0);
+        cost[1] = A.get(0) + A.get(1);
+        for(int i = 2; i < A.size(); i++) {
+            cost[i] = Math.min(cost[i-1], cost[i-2]) + A.get(i);
+        }
+        return cost[A.size() - 1];
+    }
+}
