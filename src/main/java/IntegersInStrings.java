ee67167db0a5f3353b21db5bd8ea7d6473cd61c1
vvaidya1 <vaibhav.vaidya11@gmail.com>
2022-06-15 14:23:10 +0100

    InterviewBit Java Solutions

---
+++ src/main/java/IntegersInStrings.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * https://www.interviewbit.com/problems/integers-in-strings/
+ * O(n) time | O(n) space - where n is the length of the input string
+ */
+public class IntegersInStrings {
+    public ArrayList<Integer> solve(String A) {
+        if (A == null || A.length() == 0)
+            return new ArrayList<>();
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (String num : A.split(","))
+            result.add(Integer.parseInt(num));
+        return result;
+    }
+}
